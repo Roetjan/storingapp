ccd5ed4f1b673466fa9cb167abc72a73d1ff8184
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-03-11 11:20:24 +0100

    adde kommas

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -18,12 +18,8 @@ $statement = $conn->prepare($query);
 
 //4. Execute
 $statement->execute([
-    ":attractie" => $attractie
-    ":capaciteit" => $capaciteit
-    ":melder" => $melder
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
 ]);
-
-$items = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
 header("Location: ../meldingen/index.php?msg=Melding Opgeslagen");
